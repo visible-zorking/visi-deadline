b2c6744acf89702d6c31d9883ef71a7404c41ce7
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-18 00:42:04 -0400

    Initial data.

--- src/custom/info.ts
+++ src/custom/info.ts
@@ -2,7 +2,7 @@
 /* Return the initial sourceloc to display. */
 export function sourceloc_start() : string
 {
-    return 'J:78:1:101:0';  // 'gverbs.zil', lines 78-100
+    return 'K:235:1:250:0';  // 'verbs.zil', lines 235-249
 }
 
 // Presentation order. Filenames must match game-info!
