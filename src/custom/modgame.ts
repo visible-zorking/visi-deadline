0044b344432798a98a745ad5da55b2fe6acbb6e2
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-22 21:16:17 -0400

    Pull goaltables.

--- src/custom/modgame.ts
+++ src/custom/modgame.ts
@@ -1,6 +1,23 @@
 import { unpack_address } from '../visi/gametypes';
-import { GnustoEngine } from '../visi/zstate';
+import { GnustoEngine, ZState } from '../visi/zstate';
 import { gamedat_routine_names, gamedat_global_names, gamedat_string_map } from '../visi/gamedat';
+
+/* Pull out the GOAL-TABLES. */
+export function get_goal_tables(engine: GnustoEngine, state: ZState): any
+{
+    let goaltables = [];
+    for (let char=0; char<8; char++) {
+        let goaltable = [];
+        for (let ix=0; ix<20; ix += 2) {
+            goaltable.push(engine.getWord(11569 + 20*char + ix));
+        }
+        goaltables.push(goaltable);
+    }
+
+    return {
+        goaltables: goaltables
+    }
+}
 
 export function show_commentary_hook(topic: string, engine: GnustoEngine): string|null
 {
